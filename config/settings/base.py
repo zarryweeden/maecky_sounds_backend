ab0be8d339e22a68974ad72828e8f1d4a966d5a6
Weeden Ochola <zarryochola@gmail.com>
2026-07-19 00:05:38 +0300

    fixing database issues

--- config/settings/base.py
+++ config/settings/base.py
@@ -363,9 +363,9 @@ LOGGING = {
 
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
